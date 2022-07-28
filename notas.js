f0c05d98bd1d103f14387334d788e3b2292ee71a
Valentina Amaya <amaya.ibanez.valentina@gmail.com>
2022-07-28 13:28:38 -0500

    agregar .git y primeros archivos

---
+++ notas.js
@@ -0,0 +1,13 @@
+//input.value = "456"; // cambiar valores 
+
+//console.log(document.createElement("img")); // crear cosas nuevas 
+
+//const img = document.createElement("img");
+//img.setAttribute("src", "https://media.istockphoto.com/id/1040315976/es/foto/mujer-busca-en-vista-de-una-cueva-de-matera-basilicata-italy.webp?s=612x612&w=is&k=20&c=KuF3fg3FF4D2QQQFV0LJf1ixy45vrBKYZO28finbmMI=");
+//console.log(img);
+
+//parrafito.append(img); // no borra contenido solo agrega cosas nuevas 
+
+// parrafote.innerHTML = ""; // borrar cosas de html 
+
+//btn.addEventListener("click", btnOnClick); para llamar un evento 
